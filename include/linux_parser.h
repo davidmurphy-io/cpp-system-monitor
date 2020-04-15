b0cacea54304084626952e1b1597623e33a3d381
David Murphy <david@davidmurphy.io>
2020-04-15 17:48:19 +0100

    Added helper functions to avoid duplication.

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -17,6 +17,10 @@ const std::string kMeminfoFilename{"/meminfo"};
 const std::string kVersionFilename{"/version"};
 const std::string kOSPath{"/etc/os-release"};
 const std::string kPasswordPath{"/etc/passwd"};
+const std::string kTimerListFilename{"/timer_list"};
+const std::string processes{"processes"};
+const std::string running_procs{"procs_running"};
+const std::string vm_data{"VmData:"};
 
 // System
 float MemoryUtilization();
@@ -49,9 +53,43 @@ long IdleJiffies();
 // Processes
 std::string Command(int pid);
 std::string Ram(int pid);
-std::string Uid(int pid);
+int Uid(int pid);
 std::string User(int pid);
 long int UpTime(int pid);
+
+template <typename T>
+T findValueByKey(std::string const &keyFilter, std::string const &filename) {
+  std::string line, key;
+  T value;
+
+  std::ifstream stream(kProcDirectory + filename);
+  if (stream.is_open()) {
+    while (std::getline(stream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == keyFilter) {
+          return value;
+        }
+      }
+    }
+  }
+  return value;
+};
+
+template <typename T>
+T getValueOfFile(std::string const &filename) {
+  std::string line;
+  T value;
+
+  std::ifstream stream(kProcDirectory + filename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> value;
+  }
+  return value;
+};
+
 };  // namespace LinuxParser
 
 #endif
