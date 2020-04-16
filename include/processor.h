33974f170c6a6ebecf564b6b17f814e4b085972c
David Murphy <david@davidmurphy.io>
2020-04-16 21:48:51 +0100

    Completed CPU utilisation calc. function.

--- include/processor.h
+++ include/processor.h
@@ -2,11 +2,28 @@
 #define PROCESSOR_H
 
 class Processor {
- public:
-  float Utilization();  // TODO: See src/processor.cpp
+  public:
+   float Utilization();  // TODO: See src/processor.cpp
+
+   void PrevUser(float user) { prev_user_ = user; };
+   void PrevNice(float nice) { prev_nice_ = nice; };
+   void PrevSystem(float system) { prev_system_ = system; };
+   void PrevIdle(float idle) { prev_idle_ = idle; };
+   void PrevIowait(float iowait) { prev_iowait_ = iowait; };
+   void PrevIRQ(float irq) { prev_irq_ = irq; };
+   void PrevSoftIRQ(float softirq) { prev_softirq_ = softirq; };
+   void PrevSteal(float steal) { prev_steal_ = steal; };
 
   // TODO: Declare any necessary private members
- private:
+  private:
+    float prev_user_;
+    float prev_nice_;
+    float prev_system_;
+    float prev_idle_;
+    float prev_iowait_;
+    float prev_irq_;
+    float prev_softirq_;
+    float prev_steal_;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,47 @@
+#include <string>
+#include <vector>
+
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  std::vector<std::string> cpu_utilisation = LinuxParser::CpuUtilization();
+
+  float curr_user = stof(cpu_utilisation[0]);
+  float curr_nice = stof(cpu_utilisation[1]);
+  float curr_system = stof(cpu_utilisation[2]);
+  float curr_idle = stof(cpu_utilisation[3]);
+  float curr_iowait = stof(cpu_utilisation[4]);
+  float curr_irq = stof(cpu_utilisation[5]);
+  float curr_softirq = stof(cpu_utilisation[6]);
+  float curr_steal = stof(cpu_utilisation[7]);
+
+  float prevIdle = prev_idle_ + prev_iowait_;
+  float idle = curr_idle + curr_iowait;
+
+  float prevNonIdle =
+      prev_user_ + prev_nice_ + prev_system_ + prev_irq_ + prev_softirq_ + prev_steal_;
+  float nonIdle = curr_user + curr_nice + curr_system + curr_irq + curr_softirq + curr_steal;
+
+  float prevTotal = prevIdle + prevNonIdle;
+  float total = idle + nonIdle;
+
+  double totald = total - prevTotal;
+
+  float idled = idle - prevIdle;
+
+  float CPU_Percentage = (totald - idled) / totald;
+
+  // Update 'prev' values
+  PrevUser(curr_user);
+  PrevNice(curr_nice);
+  PrevSystem(curr_system);
+  PrevIdle(curr_idle);
+  PrevIowait(curr_iowait);
+  PrevIRQ(curr_irq);
+  PrevSoftIRQ(curr_softirq);
+  PrevSteal(curr_steal);
+
+  return CPU_Percentage;
+}
